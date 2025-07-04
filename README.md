2251d5631c2de36e177ea99239432fea1259c682
Val Baca <valbaca@gmail.com>
2025-07-04 11:55:21 -0700

    init: command !archive is working

---
+++ README.md
@@ -0,0 +1,11 @@
+## Auto Archive Link
+
+A simple discord bot that provides archive.is links for urls
+
+If a link is posted that matches the [specified domains](), then prepends `https://archive.is/newest/` to the URL and posts it: `https://archive.is/newest/{original_url}` like https://archive.is/newest/http://www.example.com/
+
+OR the bot can be manually invoked with !archive
+
+## TODO
+
+- [ ] Provide a way for people to react / reply and have a domain be added to the *specified domains*
--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+import dotenv
+import os
+import discord
+from discord.ext import commands
+
+description = """
+A simple discord bot that provides archive.is links for urls
+"""
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", description=description, intents=intents)
+
+prefix = "https://archive.is/newest/"
+
+
+@bot.command()
+async def archive(ctx, url: str):
+    await ctx.send(prefix + url)
+
+
 def main():
-    print("Hello from auto-archive-link!")
+    dotenv.load_dotenv()
+    DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+    bot.run(DISCORD_TOKEN)
 
 
 if __name__ == "__main__":
