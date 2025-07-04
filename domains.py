2afaa65d3cec0c7648ebac4c0822b01082e4c4cc
Val Baca <valbaca@gmail.com>
2025-07-04 12:56:59 -0700

    v1.0.0 done

---
+++ domains.py
@@ -0,0 +1,5 @@
+domains_to_archive = (
+    # just the domain as "example.com", no protocol like http:// nor subdomain like www
+    "reuters.com",
+    "theguardian.com",
+)
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import logging
+from logging import getLogger
+import re
 import dotenv
 import os
 import discord
 from discord.ext import commands
+import tldextract
+from domains import domains_to_archive
+from urllib.parse import urlparse
 
 description = """
 A simple discord bot that provides archive.is links for urls
@@ -14,9 +20,47 @@ bot = commands.Bot(command_prefix="!", description=description, intents=intents)
 prefix = "https://archive.is/newest/"
 
 
+def to_archive_link(url: str) -> str:
+    return prefix + url
+
+
+url_pattern = r"https?://(?:www\.)?\S+|www\.\S+"
+
+
+def should_archive_link(url: str) -> bool:
+    """Returns whether the given url should get an archive link (to avoid spam)"""
+    try:
+        extracted = tldextract.extract(url)
+        if not extracted:
+            return False
+    except:
+        return False
+    domain = f"{extracted.domain}.{extracted.suffix}"
+    if domain in domains_to_archive:
+        # matched!
+        return True
+    return False
+
+
 @bot.command()
 async def archive(ctx, url: str):
-    await ctx.send(prefix + url)
+    """Allows manual invocation: !archive url"""
+    await ctx.send(to_archive_link(url))
+
+
+@bot.event
+async def on_message(message: discord.Message):
+    """Checks the message for a link with a specified domain and if matches, replies with archive url"""
+    if message.author == bot.user:
+        # avoid infinite loop!
+        return
+    urls = re.findall(url_pattern, message.content.lower())
+    if not urls:
+        return
+    for url in urls:
+        if should_archive_link(url):
+            await message.reply(to_archive_link(url))
+    return
 
 
 def main():
